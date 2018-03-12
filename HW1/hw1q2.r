149cee8c4cdd49192f833b51bda07eed499f9906
Alvin <cryolite@ntu.im>
2018-03-13 03:50:49 +0800

    hw1 done

--- HW1/hw1q2.r
+++ HW1/hw1q2.r
@@ -1,6 +1,7 @@
 mle_update <- function(mu_prev, cov_prev, n_prev, xn){
     n = n_prev + 1
     mu = mu_prev + (xn - mu_prev)/n
-    s = (cov_prev * (n-1) + ((n-1)/n) * (xn - mu_prev) %*%  t(xn - mu_prev)) / n
-    return(list(n = n, mu = mu, s = s))
+    s = (cov_prev  + (1/n) * ((xn - mu_prev) %*%  t(xn - mu_prev))) * (n_prev/n)
+    ret = list(mu = mu, s = s, n = n)
+    return(ret)
 }
